17fb9db29f1d437c8ca487bee65b096da4680785
Nelson Foltz <nelsfoltz@gmail.com>
2020-10-07 09:35:40 -0400

    add: randomNumber method

--- src/Review.jsx
+++ src/Review.jsx
@@ -30,6 +30,15 @@ const Review = () => {
     });
   };
 
+  const randomPerson = () => {
+    let randomNumber = Math.floor(Math.random() * people.length);
+    console.log(randomNumber);
+    if (randomNumber === index) {
+      randomNumber = index + 1;
+    }
+    setIndex(checkNumber(randomNumber));
+  };
+
   return (
     <article className="review">
       <div className="img-container">
@@ -49,7 +58,9 @@ const Review = () => {
           <FaChevronRight />
         </button>
       </div>
-      <button className="random-btn">surprise me</button>
+      <button className="random-btn" onClick={randomPerson}>
+        surprise me
+      </button>
     </article>
   );
 };
